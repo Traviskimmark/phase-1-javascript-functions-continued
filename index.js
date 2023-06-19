3f31a56a08be56f356be4716a6bed926a01b998a
traviskimmark <traviskimmark@gmail.com>
2023-06-19 21:45:16 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `On Saturdays, I like to ${activity}!`;
+  }
+  
+  function mondayWork(task = 'go to the office') {
+    return `On Mondays, I have to ${task}.`;
+  }
+  
+  function wrapAdjective(flair = '*') {
+    return function (adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  
+  console.log(saturdayFun()); 
+  console.log(saturdayFun('go hiking')); 
+  
+  console.log(mondayWork()); 
+  console.log(mondayWork('attend meetings')); 
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction()); 
+  console.log(encouragingPromptFunction("a dedicated programmer")); 
+  
